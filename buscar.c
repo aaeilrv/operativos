382187db1d060c911a06d43489f0723752d959bf
aaeilrv <16-11233@usb.ve>
2022-10-30 22:56:23 -0400

    prints results

--- buscar.c
+++ buscar.c
@@ -24,11 +24,10 @@ int compareA(const void *a, const void *b);
 void organizePairs(ArrayP *b, char* type);
 
 void SearchAlg(char* key, char* type, ArrayT *a, ArrayP *b);
-void BinarySearch(ArrayP *a, char* key, char* type);
+void BinarySearch(ArrayT* a, ArrayP *b, char* key, char* type);
 void LinearSearch(ArrayT *a, char* key, char* type);
 
 /** ARREGLO DINAMICO DE TRIPLETAS **/
-
 void initArray_T(ArrayT *array, int initSize) {
     array->size = initSize;
     array->used = 0;
@@ -47,7 +46,7 @@ void insertArray_T(ArrayT *array, struct Triplet* value) {
 void initArray_P(ArrayP *array, int initSize) {
     array->size = initSize;
     array->used = 0;
-    array->data = malloc(10 * sizeof(Triplet));
+    array->data = malloc(initSize * sizeof(Triplet));
 }
 
 void insertArray_P(ArrayP *array, struct Pair* value) {
@@ -81,7 +80,7 @@ Triplet* createTriplet(char* key, char* name, char* age, char* type) {
 
     if (strcmp(type, "a") == 0) {
         newTriplet->keyA = key;
-        newTriplet->keyN = 0;
+        newTriplet->keyN = -1;
         newTriplet->name = name;
         newTriplet->age = atoi(age);
     } else if (strcmp(type, "n") == 0) {
@@ -129,7 +128,10 @@ void createRecords(char filename[], struct ArrayT* a, struct ArrayP* b, char* ty
 
     fp = fopen(filename, "r");
 
-    /*agregar vaina por si no se abre el archivo o si no existe*/
+    if (fp == NULL) {
+        printf("Error al abrir el archivo");
+        exit(1);
+    }
 
     while(fgets(linea, sizeof(linea) + 1, fp)) {
         char* key = cutLine(linea, 0, 6);
@@ -154,6 +156,11 @@ void searchKeys(char filename[], struct ArrayT* a, struct ArrayP* b, char* type)
     FILE *fp;
     char linea[6];
     fp = fopen(filename, "r");
+
+    if (fp == NULL) {
+        printf("Error al abrir el archivo");
+        exit(1);
+    }
 
     while(fgets(linea, sizeof(linea) + 1, fp)) {
         char* key = cutLine(linea, 0, 6);
@@ -184,7 +191,27 @@ void organizePairs(ArrayP *b, char* type) {
     }
 }
 
+/* IMPRESION*/
+void BinaryPrint(ArrayT* a, char* keyA, int keyN, int position, char* type) {
+    if (strcmp(type, "a") == 0) {
+        printf("binary: %s: (%s, %s, %d)\n", keyA, a->data[position].keyA, a->data[position].name, a->data[position].age);
+    } else if (strcmp(type, "n") == 0) {
+        printf("binary: %d: (%d, %s, %d)\n", keyN, a->data[position].keyN, a->data[position].name, a->data[position].age);
+    } else if (strcmp(type, "c") == 0) {
+        printf("binary: %s, %d: (No existe)\n", keyA, keyN);
+    }
+}
 
+void LinearPrint(ArrayT* a, char* keyA, int keyN, char* type, int i) {
+    if (strcmp(type, "a") == 0) {
+        printf("linear: %s: (%s, %s, %d)\n", keyA, a->data[i].keyA, a->data[i].name, a->data[i].age);
+    } else if (strcmp(type, "n") == 0) {
+        printf("linear: %d: (%d, %s, %d)\n", keyN, a->data[i].keyN, a->data[i].name, a->data[i].age);
+        
+    } else if (strcmp(type, "c") == 0) {
+        printf("linear: %s, %d: (No existe)\n", keyA, keyN);
+    }
+}
 /** ALGORITMOS DE BÚSQUEDA **/
 void SearchAlg(char* key, char* type, ArrayT *a, ArrayP *b) {
 
@@ -194,10 +221,11 @@ void SearchAlg(char* key, char* type, ArrayT *a, ArrayP *b) {
     }
 
     LinearSearch(a, key, type);
-    BinarySearch(b, key, type);
+    BinarySearch(a, b, key, type);
+    printf("\n");
 }
 
-void BinarySearch(ArrayP *b, char* key, char* type) {
+void BinarySearch(ArrayT* a, ArrayP* b, char* key, char* type) {
     int i = 0;
     int j = b->used - 1;
     int m = 0;
@@ -208,7 +236,7 @@ void BinarySearch(ArrayP *b, char* key, char* type) {
 
         if (strcmp(type, "a") == 0) {
             if (strcmp(b->data[m].keyA, key) == 0) {
-                printf("(%s, %d);\n", b->data[m].keyA, b->data[m].position);
+                BinaryPrint(a, b->data[m].keyA, b->data[m].keyN, b->data[m].position, type);
                 found = 1;
             } else if (strcmp(b->data[m].keyA, key) < 0) {
                 i = m + 1;
@@ -217,7 +245,7 @@ void BinarySearch(ArrayP *b, char* key, char* type) {
             }
         } else if (strcmp(type, "n") == 0) {
             if (b->data[m].keyN == atoi(key)) {
-                printf("(%d, %d);\n", b->data[m].keyN, b->data[m].position);
+                BinaryPrint(a, b->data[m].keyA, b->data[m].keyN, b->data[m].position, type);
                 found = 1;
             } else if (b->data[m].keyN < atoi(key)) {
                 i = m + 1;
@@ -225,6 +253,10 @@ void BinarySearch(ArrayP *b, char* key, char* type) {
                 j = m - 1;
             }
         }
+    }
+
+    if (!found) {
+        BinaryPrint(a, key, atoi(key), b->data[m].position, "c");
     }
 }
 
@@ -235,27 +267,24 @@ void LinearSearch(ArrayT *a, char* key, char* type) {
     if (strcmp(type, "a") == 0) {
         while (i < a->used && !found) {
             if (strcmp(a->data[i].keyA, key) == 0) {
-                printf("Nombre: %s\n", a->data[i].name);
-                printf("Edad: %d\n", a->data[i].age);
+                LinearPrint(a, a->data[i].keyA, i, type, i);
                 found = 1;
             }
             i++;
         }
         if (!found) {
-            printf("No se encontró el registro.\n");
+            printf("Linear: No se encontró el registro.\n");
         }
     } else if (strcmp(type, "n") == 0) {
         while (i < a->used && !found) {
             if (a->data[i].keyN == atoi(key)) {
-                printf("Nombre: %s\n", a->data[i].name);
-                printf("Edad: %d\n", a->data[i].age);
+                LinearPrint(a, a->data[i].keyA, a->data[i].keyN, type, i);
                 found = 1;
             }
             i++;
         }
         if (!found) {
-            printf("searched key: %d\n", atoi(key));
-            printf("No se encontró el registro.\n");
+            LinearPrint(a, key, atoi(key), "c", 0);
         }
     }
 }
